4079e2e814f01274b074f4c3d2e005fa2f7b6f4a
Kafter <115289409+inf2021090@users.noreply.github.com>
2023-05-16 16:10:33 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ x=0.1
 for ((i=0; i<19; i++))
 do
   w=$(bc -l <<< "scale=2; 1/$x")  # Calculates 1/x using 'bc' command
-  sed -i "s/^[0-9.]\+/$(printf "%.2f" $w)/" mm1.in  # Replaces the first numeric value with the calculated result
+  sed -i "0,/[0-9]\+\.[0-9]\+/s//[0-9]\.$(printf "%02d" $w)/" mm1.in  # Replaces the first numeric value with the calculated result
   x=$(bc -l <<< "$x+0.1")  # Increment x by 0.1
 done
